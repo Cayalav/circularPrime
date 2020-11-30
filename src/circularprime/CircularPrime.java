1e637845734092ae687e2196c802a40bbfc743a5
CARLOS AYALA VISBAL <cayala3@cuc.edu.co>
2020-11-30 15:45:25 -0500

    Ya estamos generando y comprobando los diferentes numeros primos

--- src/circularprime/CircularPrime.java
+++ src/circularprime/CircularPrime.java
@@ -7,9 +7,17 @@ public class CircularPrime {
 
     public static void main(String args[]) {
         
+        int primesSize = 1000000; // Declaramos nuestro numero limite para limitar el conjunto
+        int[] primeNumbers = new int[primesSize];
+        for (int i = 0; i < primesSize; i++) { //Generación de numeros primos con optimo comprobador
+            if (esPrimo(BigInteger.valueOf(i))) {
+                primeNumbers[i] = i;
+                System.out.println(i);
+            }
+        }
     }
         
-    public Boolean esPrimo(BigInteger bi){ //Comprobador de primalidad con probabilidad
+    public static Boolean esPrimo(BigInteger bi){ //Comprobador de primalidad con probabilidad
         Boolean b2, b3;
         b2 = bi.isProbablePrime(1);
         b3 = bi.isProbablePrime(-1);
